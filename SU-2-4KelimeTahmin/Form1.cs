cb6f369d8897fe8451ec677b219df5aa62c711c5
Amantur <amantur.pro100@gmail.com>
2021-06-16 12:11:02 +0600

    Добавьте файлы проекта.

---
+++ SU-2-4KelimeTahmin/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SU_2_4KelimeTahmin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string kelimem,gizli;
+        amantur m = new amantur();
+
+        int sayac;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (sayac<3)
+            {
+                if (textBox1.Text != "")
+                {
+                    char harfim = Convert.ToChar(textBox1.Text);
+                    textBox1.Clear();
+                    gizli = m.dene(harfim, kelimem, gizli);
+                    label1.Text = gizli;
+                    if (kelimem == gizli)
+                        MessageBox.Show("Bildiniz");
+                }
+                sayac++;
+            }
+            else
+            {
+                MessageBox.Show("20 hakkınız bitti");
+                textBox1.Enabled = false;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string tahmin = textBox2.Text;
+            if (kelimem == tahmin)
+            {
+                label1.Text = tahmin;
+                MessageBox.Show("Bildiniz");
+                textBox2.Clear();
+                textBox1.Enabled = false;
+            }
+            else
+            { 
+                MessageBox.Show("Yanlış");
+                textBox2.Enabled = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           DialogResult secim= MessageBox.Show("Emin misiniz?", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (secim==DialogResult.Yes) Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+            this.Text = "KELİME TAHMİN-MaNaS-V1.0";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayac = 0;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox1.Clear();
+            textBox2.Clear();
+            bool secim;
+            if (checkBox1.Checked == true)
+                secim = true;
+            else
+                secim = false;
+            kelimem=m.kelime_getir(secim);
+            kelimem = kelimem.Trim();//boşlukları karakterlerini temizledik
+            gizli=m.kelime_gizle(kelimem);
+            label1.Text = gizli;//bastığımız kelimeyi labela gönderdik
+        }
+    }
+}
---
+++ SU-2-4KelimeTahmin/amantur.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SU_2_4KelimeTahmin
+{
+    
+    class amantur
+    {
+        SqlConnection bag = new SqlConnection("server=DESKTOP-DNDEKN8\\SQLEXPRESS;initial catalog=su2kelimetahmin;integrated security=true");
+        public string kelime_getir(bool en)
+        {
+            string kelime;
+            SqlDataAdapter da = new SqlDataAdapter("select tr,en from kelimeler", bag);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Random rasgele = new Random();
+            int satiradedi = dt.Rows.Count;
+            int sayi = rasgele.Next(0,satiradedi);
+            if (en == false)
+                kelime = dt.Rows[sayi][0].ToString();
+           else
+            kelime = dt.Rows[sayi][1].ToString();
+            return kelime;
+        }
+        public string kelime_gizle(string kelime)
+        {
+            string gizli="";
+            for (int i=0;i<kelime.Length;i++)
+            {
+                 gizli += "*";
+            }
+            return gizli;
+        }
+        public string dene(char harf,string kelime,string gizli)
+        {
+            string yeni="";
+            for (int i=0;i<kelime.Length;i++)
+            {
+                if (kelime[i] == harf)
+                    yeni += harf;
+                else
+                    yeni += gizli[i];
+            }
+            
+            return yeni;
+        }
+    }
+}
